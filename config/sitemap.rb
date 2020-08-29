f4924641ced4471c8f098fe8d2084afd821ba093
kenta-crv <adp@crold-valieat.jp>
2020-08-29 19:18:44 +0900

    sitemap

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -1,5 +1,5 @@
 # Set the host name for URL creation
-SitemapGenerator::Sitemap.default_host = "http://eatery-japan.com"
+SitemapGenerator::Sitemap.default_host = "http://xn--u9jy72glt5bkkf.net"
 
 SitemapGenerator::Sitemap.sitemaps_path = 'sitemaps/'
 
